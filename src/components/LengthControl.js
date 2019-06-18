e271ed4613786593a16298fa37f742dc9f6296cc
Tina <radostinadimitrova@gmail.com>
2019-06-18 15:53:51 +0300

    refactor: Change structure for the styles

--- src/components/LengthControl.js
+++ src/components/LengthControl.js
@@ -2,39 +2,41 @@ import React, { Component } from 'react'
 
 class LengthControl extends Component {
   render() {
-    const { minSession, onChange, incrementSession, decrementSession, minBreak, incrementBreak, decrementBreak } = this.props;
+    const { minSession, incrementSession, decrementSession, minBreak, incrementBreak, decrementBreak } = this.props;
     return (
       <div>
         <div className="length-control">
           <div id="break-label">Break Length</div>
-          <button 
-          id="break-decrement"
-          min={minBreak}
-          onChange={onChange}
-          onClick={decrementBreak}
-          >-</button>
-          <div id="break-length" onChange={onChange}>{minBreak}</div>
-          <button 
-          id="break-increment"
-          min={minBreak}
-          onClick={incrementBreak}
-          >+</button>
+          <div className="control-buttons">
+            <button 
+            id="break-decrement"
+            min={minBreak}
+            onClick={decrementBreak}
+            >-</button>
+            <div id="break-length">{minBreak}</div>
+            <button 
+            id="break-increment"
+            min={minBreak}
+            onClick={incrementBreak}
+            >+</button>
+          </div>
         </div>
 
         <div className="length-control">
           <div id="session-label">Session Length</div>
-          <button 
-          id="session-decrement"
-          minutes={minSession}
-          onClick={decrementSession}
-          >-</button>
-          <div id="session-length" onChange={onChange}>{minSession}</div>
-          <button 
-          id="session-increment"
-          minutes={minSession}
-          // onChange={onChange}
-          onClick={incrementSession}
-          >+</button>
+          <div className="control-buttons">
+            <button 
+            id="session-decrement"
+            minutes={minSession}
+            onClick={decrementSession}
+            >-</button>
+            <div id="session-length">{minSession}</div>
+            <button 
+            id="session-increment"
+            minutes={minSession}
+            onClick={incrementSession}
+            >+</button>
+          </div>
         </div>
       </div>
     )
